4a36e9dae4e3d9b0a01371ff1363ae421aa99746
Andrej Baranovskij <andrejus.baranovskis@gmail.com>
2022-11-28 16:05:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     version="0.1.0",
     author="Andrej Baranovskij",
     author_email="andrejus.baranovskis@gmail.com",
-    description="Streamlit component for invoice document labeling.",
+    description="Streamlit component for invoice document labeling",
     long_description=readme(),
     long_description_content_type="text/markdown",
     url="https://github.com/katanaml/streamlit-sparrow-labeling-comp",
